a37dae7a5b395c6064c93d2cc9f6cc2b53b35154
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2023-06-06 00:23:48 +0700

    Implement useState to add product to cart

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,4 +1,4 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import CardProduct from "../components/Fragments/CardProduct";
 import Button from "../components/Elements/Button";
 
@@ -6,7 +6,7 @@ const products = [
   {
     id: 1,
     name: "Sepatu Baru",
-    price: "Rp 5.000.000",
+    price: 5000000,
     image: "/images/shoes-1.jpg",
     description: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ea
         voluptatibus mollitia ex laborum quaerat ut. Quisquam
@@ -16,14 +16,14 @@ const products = [
   {
     id: 2,
     name: "Sepatu Lama",
-    price: "Rp 2.000.000",
+    price: 2000000,
     image: "/images/shoes-1.jpg",
     description: `Lorem ipsum dolor, sit amet consectetur adipisicing elit.`,
   },
   {
     id: 3,
     name: "Sepatu Super",
-    price: "Rp 3.000.000",
+    price: 3000000,
     image: "/images/shoes-1.jpg",
     description: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa quia quibusdam debitis molestiae aperiam amet!`,
   },
@@ -32,10 +32,29 @@ const products = [
 const email = localStorage.getItem("email");
 
 const ProductsPage = () => {
+  const [cart, setCart] = useState([
+    {
+      id: 1,
+      qty: 1,
+    },
+  ]);
+
   const handleLogout = () => {
     localStorage.removeItem("email");
     localStorage.removeItem("password");
     window.location.href = "/login";
+  };
+
+  const handleAddToCart = (id) => {
+    if (cart.find((item) => item.id === id)) {
+      setCart(
+        cart.map((item) =>
+          item.id === id ? { ...item, qty: item.qty + 1 } : item
+        )
+      );
+    } else {
+      setCart([...cart, { id, qty: 1 }]);
+    }
   };
 
   return (
@@ -47,18 +66,66 @@ const ProductsPage = () => {
         </Button>
       </div>
       <div className="flex justify-center py-5">
-        <div className="w-3/4 flex flex-w">
+        <div className="w-4/6 flex flex-w">
           {products.map((product) => (
             <CardProduct key={product.id}>
               <CardProduct.Header image={product.image} />
               <CardProduct.Body name={product.name}>
                 {product.description}
               </CardProduct.Body>
-              <CardProduct.Footer price={product.price} />
+              <CardProduct.Footer
+                price={product.price}
+                id={product.id}
+                handleAddToCart={handleAddToCart}
+              />
             </CardProduct>
           ))}
         </div>
-        <div className="w-1/4"></div>
+        <div className="w-2/6">
+          <h1 className="text-3xl font-bold text-blue-600 ml-5 mb-2">
+            Cart
+          </h1>
+          <table className="text-left table-auto border-separate border-spacing-x-5">
+            <thead>
+              <tr>
+                <th>Product</th>
+                <th>Price</th>
+                <th>Quantity</th>
+                <th>Total</th>
+              </tr>
+            </thead>
+            <tbody>
+              {cart.map((item) => {
+                const product = products.find(
+                  (product) => product.id === item.id
+                );
+                return (
+                  <tr key={item.id}>
+                    <td>{product.name}</td>
+                    <td>
+                      Rp{" "}
+                      {product.price.toLocaleString("id-ID", {
+                        styles: "currency",
+                        currency: "IDR",
+                      })}
+                    </td>
+                    <td>{item.qty}</td>
+                    <td>
+                      Rp{" "}
+                      {(product.price * item.qty).toLocaleString(
+                        "id-ID",
+                        {
+                          styles: "currency",
+                          currency: "IDR",
+                        }
+                      )}
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
       </div>
     </Fragment>
   );
